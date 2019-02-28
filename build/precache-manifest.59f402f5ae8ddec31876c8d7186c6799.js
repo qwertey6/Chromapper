12b30b1bbaf9c91197fcb3f2fa0763edae5a676d
Jacob Kaplan <jtkaplan@wpi.edu>
2019-02-27 21:06:22 -0500

    Build

---
+++ build/precache-manifest.59f402f5ae8ddec31876c8d7186c6799.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "19365b498bd49bb93831",
+    "url": "/static/js/main.19365b49.chunk.js"
+  },
+  {
+    "revision": "858dfacbd6a13222479f",
+    "url": "/static/js/2.858dfacb.chunk.js"
+  },
+  {
+    "revision": "19365b498bd49bb93831",
+    "url": "/static/css/main.e1971235.chunk.css"
+  },
+  {
+    "revision": "f7a83ac96c9be62282d2efd305c2dc05",
+    "url": "/index.html"
+  }
+];
