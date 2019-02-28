2e51f45f613230db5508e31adc6d85ebdce2bb92
Jacob Kaplan <jtkaplan@wpi.edu>
2019-02-27 20:51:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -112,6 +112,6 @@ firebaser.fetchCache((data)=>{
 process.on('SIGTERM', shutDown);
 process.on('SIGINT', shutDown);
 
-setInterval(()=>{firebaser.saveCache(delegator)},6000);// auto-save the delegator every minute
+setInterval(()=>{firebaser.saveCache(delegator)},120000);// auto-save the delegator every two minutes
 
 const server = app.listen(process.env.PORT || port);
